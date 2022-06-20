cf9ee18a5a3403ca8f527dea396b16096af94e14
karthik <karthikhallad@gmail.com>
2022-06-21 02:34:10 +0530

    first message

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+
 app.get('/', (req, res) =>{
     res.send("hello")
+})
+
+app.get('/createMeeting', (req, res) =>{
+    const token = getToken;
+    const url= createMeeting(token);
+    alert(url);
 })
 
 app.get("/api", (req, res) => {
