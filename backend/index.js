ac9755539d03c695e1b0fb33a4880924ec210990
karthik <karthikhallad@gmail.com>
2022-06-20 11:52:32 +0530

    karthikpush1

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) =>{
+    res.send("hello")
+})
+
 app.get("/api", (req, res) => {
     res.json({
         "users":["userOne", "userTwo", "userThree"]
